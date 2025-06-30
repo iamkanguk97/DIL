b57e3f3ddcc9270e042f30ec764a9e407a1eb19b
KANGUK LEE <iamkanguk97@kakao.com>
2025-06-30 16:33:40 +0900

    refactor: 리팩터링 - 디렉토리 및 파일 구조 변경 및 switch 문 함수 추출

---
+++ Refactoring/Chapter01/examples/statement/2-2.js
@@ -0,0 +1,77 @@
+/**
+ * 2-2) switch 내에 포함된 로직 분리하기 + 중첩 함수로 사용하기
+ * - for문 내의 변수이기 때문에 중첩함수가 접근할 수가 없습니다.
+ */
+
+const fs = require('fs');
+
+/**
+ * 공연료 청구서를 출력하는 함수
+ * @param {Object} invoice 공연료 청구서
+ * @param {Object} plays 공연 정보
+ */
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
+  // Intl: JavaScript의 국제화(Internationalization) API
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format;
+
+  /**
+   * 회극/비극에 따라 amount 계산하는 함수
+   * @param {Object} perf 기본 공연 정보에 대한 정보
+   * @param {Object} play 기본 공연 정보
+   * @returns {number} 공연 요금
+   */
+  function amountFor(perf, play) {
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case 'tragedy': // 비극
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case 'comedy': // 희극
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
+    return thisAmount;
+  }
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = amountFor(perf, play); // 중첩함수여도 어짜피 매개변수로 보내야됨!
+
+    volumeCredits += Math.max(perf.audience - 30, 0); // 포인트 적립
+    if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5); // 희극 관객 5명마다 추가포인트 지급
+
+    result += `${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += thisAmount;
+  }
+
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+}
+
+// Main
+const plays = JSON.parse(fs.readFileSync('./data/plays.json', 'utf8'));
+const invoices = JSON.parse(fs.readFileSync('./data/invoices.json', 'utf8'));
+
+console.log(statement(invoices[0], plays));

5e733bcffeb217f40c00df4e7132a2807da49d24
KANGUK LEE <iamkanguk97@kakao.com>
2025-06-30 16:10:59 +0900

    docs: 리팩터링 챕터1 - 시작 내용 작성

---
+++ Refactoring/Chapter01/examples/statement-1-basic.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+
+/**
+ * 공연료 청구서를 출력하는 함수
+ * @param {Object} invoice 공연료 청구서
+ * @param {Object} plays 공연 정보
+ */
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+ 
+  // Intl: JavaScript의 국제화(Internationalization) API
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case 'tragedy':   // 비극
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case 'comedy':    // 희극
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
+
+    volumeCredits += Math.max(perf.audience - 30, 0);   // 포인트 적립
+    if (play.type === 'comedy') volumeCredits += Math.floor(perf.audience / 5);   // 희극 관객 5명마다 추가포인트 지급
+    
+    result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
+    totalAmount += thisAmount;
+  }
+
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
+}
+
+// Main
+const plays = JSON.parse(fs.readFileSync("./plays.json", "utf8"));
+const invoices = JSON.parse(fs.readFileSync("./invoices.json", "utf8"));
+
+console.log(statement(invoices[0], plays));
